bbb2cf34e84d2c441f7267bcc3f23a3eee01a4bc
Mary <mary.becica@appdirect.com>
2012-01-03 12:04:40 -0800

    cleanup and use custom attributes

--- js/plumpette.js
+++ js/plumpette.js
@@ -1,6 +1,13 @@
+/* 
+ * Plumpette is a bite-sized version of plump, an icon management library
+ * Customized for AppDirect
+ * Created by Kai Chang and Mary Becica, 2011
+ * plumpjs.com
+ * 
+ * */
+
 var plumpette = (function (Raphael) {
-	//Plumpette is a bite-sized version of plump, an icon management library
-	var plumpette = {};
+    var plumpette = {};
 
     /****************/
     // Helper functions
@@ -32,20 +39,24 @@ var plumpette = (function (Raphael) {
         attrs.fill = attrs.fill || '#333';
 
 		//Target drawing
-		var canvas = $('.'+className)[0];
+		var paper = className[0],
+        offsetX = -(className.outerWidth()-className.width())/2,
+        offsetY = -(className.outerHeight()-className.height())/2;
 
         // Non-standard attributes
-        attrs.size = attrs.size || 24;
-		attrs.sizeW = attrs.size || canvas.width();
-		attrs.sizeH = attrs.size || canvas.height();
-        attrs.glow = attrs.glow || 'transparent';
-        attrs['glow-width'] = attrs['glow-width'] || 2;
+        attrs.size = attrs.size || 20;
+		    attrs.sizeW = attrs.size || paper.width;
+		    attrs.sizeH = attrs.size || paper.height;
+        attrs.transW = offsetX;
+        attrs.transH = offsetY;
         if (attrs.hover) {
             attrs.hover.duration = attrs.hover.duration || 0;
         }	
-        var r = Raphael(canvas, attrs.sizeW, attrs.sizeH),
-            s = r.path(ADicons[name]).translate(2, 2).attr({fill: 'none', stroke: attrs.glow, 'stroke-width': attrs['glow-width']}),
-        	icon = r.path(ADicons[name]).translate(2, 2).attr(attrs);
+        var r = Raphael(paper, attrs.sizeW, attrs.sizeH);
+        if (attrs.blur) {
+        	var s = r.path(ADicons[name]).attr(attrs).attr({fill:'#ccc'}).scale(.9, .9).blur(attrs.blur);
+        }
+        var icon = r.path(ADicons[name]).translate(attrs.transW, attrs.transH).attr(attrs);
         var target = parentTarget || r.rect(0, 0, attrs.sizeW, attrs.sizeH).attr({stroke:'none'});
         target.click(function () {
 		      if (attrs.click) {
@@ -56,7 +67,7 @@ var plumpette = (function (Raphael) {
 		          icon.animate(attrs.hover, attrs.hover.duration);
 		      }
 		    }, function () {
-		      icon.stop().attr(attrs);
+		      icon.stop().attr({fill:attrs.fill, stroke:attrs.stroke, opacity:attrs.opacity});
 		    });
       return { id: id++
                , icon: icon
@@ -73,20 +84,22 @@ var plumpette = (function (Raphael) {
                        }
                , translate: function(x, y) {
                           target.translate(x,y);
-                          s.translate(x,y);
                           icon.translate(x,y);
+                          if (s) {
+                        	  s.translate(x,y);
+                          }
                           return this;
                        }
                , hover: function(f) {
                           target.hover(f);
                           return this;
                       }
-		, rotate: function(degree) {
-			icon.rotate(degree);
-			return this;
-			}
+				, rotate: function(degree) {
+						icon.rotate(degree);
+						return this;
+					}
                , draggable: function() { return draggable(this);}
-               }
+               };
 
     };
 
@@ -97,6 +110,19 @@ var plumpette = (function (Raphael) {
 			}
 		}
 	};
-
+  
+  var starRow = plumpette.starRow = function(icons, className, target, attrs) {
+    var attrs = attrs || {};
+    for (var i=0;i<icons.length;i++) {  
+        for (var name in icons[i]) {
+            if (icons[i].hasOwnProperty(name)) {
+            	className.append("<span class='iconSvg' id='pos"+i+"'></span>");
+                icon(name, className.find('#pos'+i), target, Object.extend(attrs, icons[i][name]));
+                attrs.x += 12;
+            }
+        }
+      }
+    };
+    
 	return plumpette;
 }(Raphael));
